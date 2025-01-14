30057eae8d9053cbc3bd44e3ae5ffeed279e9e01
sunub <bsc5672@gmail.com>
2025-01-14 17:11:00 +0900

    refactor : 불필요한 인증 확인 절차 제거거

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,8 +3,7 @@ import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { UserProvider } from "@/app/provider/UserProvider";
 import { GlobalErrorComponent } from "@/shared/components/Error/GlobalError";
 import { LayoutProvider } from "@/app/provider/LayoutProvider";
-import { RouterContext } from "@/main";
-import { authQueries } from "@/shared/lib/auth/authQuery";
+import { RouterContext } from "@/app/provider/RouterProvider";
 import { LoadingAnimation } from "@/shared/components/Loading";
 import { Layout } from "@/app/layout";
 
@@ -20,6 +19,5 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       </UserProvider>
     </LayoutProvider>
   ),
-  loader: (opts) => opts.context.queryClient.ensureQueryData(authQueries),
   errorComponent: ({ error }) => <GlobalErrorComponent error={error} to="/" />,
 });
