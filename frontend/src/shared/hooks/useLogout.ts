947c6b86a25686f88ad8a640da7d345c57fae264
sunub <bsc5672@gmail.com>
2025-01-21 22:17:46 +0900

    feat : 중복되는 코드를 커스텀 훅으로 관리하게끔 코드 추가

--- frontend/src/shared/hooks/useLogout.ts
+++ frontend/src/shared/hooks/useLogout.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 import { useCallback } from "react";
-import { useNavigate } from "@tanstack/react-router";
 import { useUserContext } from "@/shared/hooks/useUserContext";
 import { useSetRecoilState } from "recoil";
 import { useQueryClient } from "@tanstack/react-query";
 import { Auth } from "@/app/provider/RouterProvider/lib/auth";
 import { authQueries } from "@/shared/lib/auth/authQuery";
 import { AuthStatusTypeSchema } from "@/shared/lib/auth/guard";
+import { useNavigate } from "@tanstack/react-router";
 
 export const useLogout = () => {
   const navigate = useNavigate();
@@ -18,47 +18,52 @@ export const useLogout = () => {
     try {
       const response = await fetch("/api/users/signout", {
         credentials: "include",
+        cache: "no-cache",
       });
 
       if (!response.ok) {
         throw new Error("로그아웃에 실패했습니다.");
       }
 
-      // Recoil 상태 업데이트
       setAuthState((prev) => ({
         ...prev,
         isAuthenticated: false,
+        isLoading: true,
         nickname: "",
       }));
 
-      // User Context 상태 업데이트
       setUserInfo({ isAuthenticated: false, nickname: "", role: "guest" });
 
-      // React Query 상태 업데이트
       queryClient.setQueryData(
         authQueries.queryKey,
-        (prev: z.infer<typeof AuthStatusTypeSchema>) => ({
-          ...prev,
-          userInfo: {
-            role: "guest",
-            nickname: "",
-            duck: 0,
-            message: "OK",
-            realDuck: prev.userInfo.realDuck,
-          },
-        }),
+        (prev: z.infer<typeof AuthStatusTypeSchema>) => {
+          console.log("prev", prev);
+          return {
+            ...prev,
+            userInfo: {
+              role: "guest",
+              nickname: "",
+              duck: 0,
+              message: "OK",
+              realDuck: prev.userInfo.realDuck,
+            },
+          };
+        },
       );
 
       await queryClient.invalidateQueries({
         queryKey: authQueries.queryKey,
       });
 
-      // 로그인 페이지로 네비게이션
-      navigate({ to: "/login" });
+      navigate({ to: "/login", replace: true });
     } catch (error) {
       console.error("Logout Error:", error);
-      // 추가적인 에러 처리 로직 (예: 사용자에게 에러 메시지 표시)
       alert("로그아웃 중 오류가 발생했습니다. 다시 시도해 주세요.");
+    } finally {
+      setAuthState((prev) => ({
+        ...prev,
+        isLoading: false,
+      }));
     }
   }, [navigate, setUserInfo, setAuthState, queryClient]);
 
