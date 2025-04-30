eb8112be9d15751bff83e2d7a37f9bf83f8a7808
정동교 <jdk8467@naver.com>
2025-04-30 14:02:24 +0900

    feat: IP 로그 기록 추가

--- backend/src/utils/middlewares/logger.middleware.ts
+++ backend/src/utils/middlewares/logger.middleware.ts
@@ -6,6 +6,8 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const { method, originalUrl } = req;
     const start = Date.now();
+    const ip =
+      req.headers["x-forwarded-for"]?.toString().split(",")[0].trim() || req.ip;
 
     res.on("finish", () => {
       const duration = Date.now() - start;
@@ -19,7 +21,7 @@ export class LoggerMiddleware implements NestMiddleware {
         hour12: false,
       });
       console.log(
-        `[Nest] ${process.pid}  - ${timestamp}       LOG [LoggerMiddleware] Mapped {${originalUrl}, ${method}} route +${duration}ms`,
+        `[Nest] ${process.pid}  - ${timestamp}       LOG [LoggerMiddleware] Mapped {${originalUrl}, ${method}} from IP ${ip} +${duration}ms`,
       );
     });
 
