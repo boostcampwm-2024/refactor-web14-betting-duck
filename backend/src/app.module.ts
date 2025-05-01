b89582d01df0f298dd636e8548156929b5cd74aa
정동교 <jdk8467@naver.com>
2025-05-01 21:21:05 +0900

    chore: 도커 빌드용 env 적용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -16,6 +16,7 @@ import { DBManagerModule } from "./utils/db.manager.module";
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: ".env.docker",
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
